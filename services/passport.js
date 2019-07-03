95510ed6799b2e39406213efca486ea961a65b4d
SnowSurfer <drinkaziu@gmail.com>
2019-07-02 23:51:56 -0400

    tweek GoogleStrategy

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,7 @@ passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
+    callbackURL: 'http://loclahost:5000/auth/google/callback',
     proxy: true
   }, (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleId: profile.id }).then((existingUser) => {
